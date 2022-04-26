29d8de7df90382f688ce383da1f17d83f0ed5e0c
salpie <senowinski@gmail.com>
2022-04-26 14:34:58 +0100

    Update content_script.js
    edited cloud to butt to elon to burns

--- Source/content_script.js
+++ Source/content_script.js
@@ -39,10 +39,10 @@ function handleText(textNode)
 {
 	var v = textNode.nodeValue;
 
-	v = v.replace(/\bThe Cloud\b/g, "My Butt");
-	v = v.replace(/\bThe cloud\b/g, "My butt");
-	v = v.replace(/\bthe Cloud\b/g, "my Butt");
-	v = v.replace(/\bthe cloud\b/g, "my butt");
+	v = v.replace(/\bElon Musk\b/g, "Mr Burns");
+	v = v.replace(/\bElon Musk\b/g, "Mr Burns");
+	v = v.replace(/\bElon Musk\b/g, "Mr Burns");
+	v = v.replace(/\bElon Musk\b/g, "Mr Burns");
 	
 	textNode.nodeValue = v;
 }
